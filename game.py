bce8282b619393f84655c5457de50ac4d9661462
Casey Tomchuk <Caseytomchuk@gmail.com>
2025-06-18 21:45:50 -0400

    Added total items

--- game.py
+++ game.py
@@ -4,7 +4,9 @@ def displayInventory(inventory):
     print("Inventory:")
     item_total = 0
     for k, v in inventory.items():
-        print(f"{v} {k}")
-    #print("Total number of items: " + str(item_total))
+        print(f"{v} {k}") # prints key and value for each item in inventory
+        itemAsInt = int(v) 
+        item_total += itemAsInt # add value to the total
+    print("Total number of items: " + str(item_total))
 
 displayInventory(stuff)
