80c49cf5afb417418da0e76b459c1b78ec623491
Casey Tomchuk <Caseytomchuk@gmail.com>
2025-06-21 17:07:27 -0400

    list to dictionary

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
 
 def displayInventory(inventory):
     print("Inventory:")
@@ -9,4 +10,13 @@ def displayInventory(inventory):
         item_total += itemAsInt # add value to the total
     print("Total number of items: " + str(item_total))
 
-displayInventory(stuff)
+def addToInventory(inventory, addedItems):
+    tempDict = {value: addedItems.count(value) for value in addedItems} #value is the number of times the item appears in loot
+    print(tempDict)
+
+addToInventory(stuff, dragonLoot) # stuff is existing inventory, dragonLoot is what is added
+#displayInventory(stuff)
+
+# LIST TO DICTIONARY
+#The addToInventory() function should return a dictionary that represents the updated inventory
+# gold coin: 3 dagger: 1 ruby: 1
